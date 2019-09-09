7c01eb25babd733e8ec166c6266fb10279416d2c
jluribe <jluribe@gmail.com>
2019-09-09 12:34:45 -0300

    replace index

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
-(function(root, factory) {
-  // Support CommonJS
-  if (typeof exports === 'object') {
-    var randomColor = factory();
-
-    // Support NodeJS & Component, which allow module.exports to be a function
-    if (typeof module === 'object' && module && module.exports) {
-      exports = module.exports = randomColor;
-    }
-
-    // Support CommonJS 1.1.1 spec
-    exports.randomColor = randomColor;
-
-    // Support AMD
-  } else if (typeof define === 'function' && define.amd) {
-    define([], factory);
-
-    // Support vanilla script loading
-  } else {
-    root.randomColor = factory();
-  }
-})(this, function() {
+(function(root, factory) {})(this, function() {
   var randomColor = function(options) {
     return `mission controller v1, options:${JSON.stringify(options)}`;
   };
-
+  var randomColor2 = function(options) {
+    return `mission controller v1, options:${JSON.stringify(options)}`;
+  };
   return randomColor;
 });

823ba2017e014f0719f60bb4e74d19c4092ef905
jluribe <jluribe@gmail.com>
2019-09-09 13:52:02 -0300

    rename to missionBuildStep with logs

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 (function(root, factory) {
-  console.log('init retool scritp - root', root);
-  console.log('init retool scritp - factory', factory);
+  console.log(
+    'retool_script - init retool scritp - root',
+    JSON.stringify(root)
+  );
+  console.log(
+    'retool_script - init retool scritp - factory',
+    JSON.stringify(factory)
+  );
   // Support CommonJS
   if (typeof exports === 'object') {
-    console.log('retool scritp start exports object');
+    console.log('retool_script - retool scritp start exports object');
     var missionBuildStep = factory();
 
     // Support NodeJS & Component, which allow module.exports to be a function
     if (typeof module === 'object' && module && module.exports) {
-      console.log('retool scritp start module object');
+      console.log('retool_script - retool scritp start module object');
       exports = module.exports = missionBuildStep;
     }
 
@@ -17,16 +23,17 @@
 
     // Support AMD
   } else if (typeof define === 'function' && define.amd) {
-    console.log('retool scritp start define function');
+    console.log('retool_script - retool scritp start define function');
     define([], factory);
 
     // Support vanilla script loading
   } else {
-    console.log('retool scritp start root');
+    console.log('retool_script - retool scritp start root');
     root.missionBuildStep = factory();
   }
-  console.log('end retool scritp');
+  console.log('retool_script - end retool scritp');
 })(this, function() {
+  console.log('retool_script - start body 1');
   var missionBuildStep = function({
     elementId,
     name,
@@ -57,6 +64,6 @@
       }
     };
   };
-
+  console.log('retool_script - start body 2');
   return missionBuildStep;
 });

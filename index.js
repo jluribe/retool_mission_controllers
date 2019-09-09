0081ff26229b33881cb14154418957c37e6b006e
jluribe <jluribe@gmail.com>
2019-09-09 14:30:56 -0300

    add new export lib

--- index.js
+++ index.js
@@ -1,35 +1,3 @@
-(function(root, factory) {
-  root.missionBuildStepAlias = factory();
-})(this, function() {
-  var missionBuildStep = function({
-    elementId,
-    name,
-    description = '',
-    position,
-    stageId,
-    defaultThumbUrl
-  }) {
-    return {
-      id: uuid.v4(),
-      element_id: elementId,
-      element_type: 'io.skore.events.content',
-      name,
-      description,
-      position,
-      stage_id: stageId,
-      controllers: [],
-      retry: {
-        enabled: false,
-        max_attempt: 0,
-        interval: 0
-      },
-      thumb_url: defaultThumbUrl,
-      success_criteria: {
-        type: 'ACCESS',
-        value: 100,
-        operator: '='
-      }
-    };
-  };
-  return missionBuildStep;
-});
+exports.missionBuildStepAlias = ({ element_id }) => {
+  return { element_id };
+};

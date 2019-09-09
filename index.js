70af93c72db92e2bcfc9fa2eefd9f2995079093f
jluribe <jluribe@gmail.com>
2019-09-09 13:59:33 -0300

    rename to missionBuildStep with logs

--- index.js
+++ index.js
@@ -1,33 +1,6 @@
 (function(root, factory) {
-  console.log('retool_script - init retool scritp - root', root);
-  console.log('retool_script - init retool scritp - factory', factory);
-  // Support CommonJS
-  if (typeof exports === 'object') {
-    console.log('retool_script - retool scritp start exports object');
-    var missionBuildStep = factory();
-
-    // Support NodeJS & Component, which allow module.exports to be a function
-    if (typeof module === 'object' && module && module.exports) {
-      console.log('retool_script - retool scritp start module object');
-      exports = module.exports = missionBuildStep;
-    }
-
-    // Support CommonJS 1.1.1 spec
-    exports.missionBuildStep = missionBuildStep;
-
-    // Support AMD
-  } else if (typeof define === 'function' && define.amd) {
-    console.log('retool_script - retool scritp start define function');
-    define([], factory);
-
-    // Support vanilla script loading
-  } else {
-    console.log('retool_script - retool scritp start root');
-    root.missionBuildStep = factory();
-  }
-  console.log('retool_script - end retool scritp');
+  root.missionBuildStepAlias = factory();
 })(this, function() {
-  console.log('retool_script - start body 1');
   var missionBuildStep = function({
     elementId,
     name,
@@ -58,6 +31,5 @@
       }
     };
   };
-  console.log('retool_script - start body 2');
   return missionBuildStep;
 });
